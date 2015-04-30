f2795946d99f15a14c1fa0116c144af14a17a370
jeremy <jeremy@kwmassage.com>
2015-04-30 19:12:14 -0400

    racial ability modifier applied

--- app/controllers/params.rb
+++ app/controllers/params.rb
@@ -1,3 +1,15 @@
+###
+# Strong Parameters
+###
+
+def ability_params
+	params.require(:ability).permit(:str, :dex, :con, :int, :wis, :char)
+end
+
+def character_id_params
+	params.require(:character_id)
+end
+
 # attr_assasable :character
 def character_params
 	params.require(:character).permit(:character_id, :character_name, :character_class, :level, :background, :player_name, :race, :alignment, :xp, :subrace)
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,45 @@
 class Ability < ActiveRecord::Base
 	belongs_to :character
-
+	before_create :add_racial_ability_modifyer
 	#valadateions
 	stats = :str, :dex, :con, :int, :wis, :char
 	validates_presence_of stats
-	validates_numericality_of stats, only_integer: true, greater_than: 3, less_than: 21
+	validates_numericality_of stats, only_integer: true, greater_than: 2, less_than: 21
+	def add_racial_ability_modifyer
+		
+		subrace = Character.find(self.character_id).subrace
+		case subrace
+		when "mountain"
+			self.str += 2 # +2 str
+			self.con += 2 # +2 con
+		when "hill"
+			self.wis += 1 # +1 wis
+			self.com += 2 # +2 con
+		when "high"
+			self.dex += 2 # +2 dex
+			self.int += 1 # +1 int
+		when "wood"
+			self.dex += 2 # +2 dex
+			self.wis += 1 # +1 wis
+		when "lightfoot"
+			self.char += 1 # +1 char
+		when "stout"
+			self.con += 1# +1 con
+		when nil # Human, does not have a sub class
+			self.str += 1
+			self.dex += 1 
+			self.con += 1
+			self.int += 1
+			self.wis += 1
+			self.char += 1
+		end
+
+	end
+	def ability_params
+		params.require(:ability).permit(:str, :dex, :con, :int, :wis, :char)
+	end
+
+
 
 
 end
