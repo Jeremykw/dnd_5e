23804d05d663337831a5e8c766391e02414f2cd0
Jeremy <jeremy@kwmassage.com>
2015-07-06 17:44:20 -0300

    working on armour categoty query

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,13 @@
 class ItemsController < ApplicationController
 
 	def index
-		armour = Item.armour.group_by { |t| t[:category] }
-		@armour = {"light" => armour["light_armour"], "medium" => armour["medium_armour"], "heavy" => armour["heavy_armour"]}
-		@weapons = Item.weapons.group_by { |t| t[:category] }
-		@adventuring_gear = Item.adventuring_gear
-		@tools = Item.tools
-		@boats = Item.boats.sort_by { |hsh| hsh[:name] }
-		@tack = Item.tack
-		@mounts = Item.mounts
+		@armour = Item.where('category', '^armour')
+		@weapons = Item.where('category', )
+		@adventuring_gear = Item.where('category', )
+		@tools = Item.where('category', )
+		@boats = Item.where('category', )
+		@tack = Item.where('category', )
+		@mounts = Item.where('category', )
 	end
 
 end
