f8d425694ea6311e55fb0a896af26d82e2096064
jeremy <jeremy@kwmassage.com>
2015-07-07 09:21:06 -0400

    items controller loads items by category, armour displayes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
 
 	def index
-		@armour = Item.where('category', '^armour')
-		@weapons = Item.where('category', )
-		@adventuring_gear = Item.where('category', )
-		@tools = Item.where('category', )
-		@boats = Item.where('category', )
-		@tack = Item.where('category', )
-		@mounts = Item.where('category', )
+		@armour = Item.where("category like ?", 'armour%')
+		@weapons = Item.where("category like ?", 'weapon%')
+		@adventuring_gear = Item.where("category like ?", 'adventuring_gear%')
+		@tools = Item.where("category like ?", 'tool%')
+		@boats = Item.where("category like ?", 'boat%')
+		@tack = Item.where("category like ?", 'tack%')
+		@mounts = Item.where("category like ?", 'mount%')
 	end
 
 end
