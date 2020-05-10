a880ade75fc346bc405c770fdb44bbe9a70dedd9
Jeremy Bissonnette <jeremy@kwmassage.com>
2020-05-10 10:39:23 -0400

    -2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-config.assets.precompile = false
+# config.assets.precompile = false
+
+config.assets.initialize_on_precompile = false
